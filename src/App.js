1ae75c495fae45ae9040878144629876e0ec998a
Nobert Momoh <okhai0112@gmail.com>
2020-02-22 21:10:42 +0100

    Created the shop page

--- src/App.js
+++ src/App.js
@@ -4,19 +4,14 @@ import { Route } from 'react-router-dom'
 import './App.css';
 
 import HomePage from '../src/pages/homepage/homepage.component'
-
-const HatsPage= () => (
-  <div>
-    <h1>Hats Page</h1>
-  </div>
-)
+import ShopPage from '../src/pages/shop/shop.component'
 
 
 function App() {
   return (
     <div>
         <Route exact path='/' component={HomePage} />
-        <Route exact path='/hats' component={HatsPage} />
+        <Route exact path='/shop' component={ShopPage} />
     </div>
   );
 }
