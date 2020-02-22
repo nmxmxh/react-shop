0f7cecf92c76450ec7e6ce7b4c6755a6162193f1
Nobert Momoh <okhai0112@gmail.com>
2020-02-22 18:51:36 +0100

    Updated the Menu with Routing

--- src/App.js
+++ src/App.js
@@ -1,14 +1,22 @@
 import React from 'react';
+import { Route } from 'react-router-dom'
 
 import './App.css';
 
 import HomePage from '../src/pages/homepage/homepage.component'
 
+const HatsPage= () => (
+  <div>
+    <h1>Hats Page</h1>
+  </div>
+)
+
 
 function App() {
   return (
     <div>
-        <HomePage />
+        <Route exact path='/' component={HomePage} />
+        <Route exact path='/hats' component={HatsPage} />
     </div>
   );
 }
