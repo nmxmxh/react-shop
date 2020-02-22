fa80d453d074aab35954ab6531e49fe22cba629b
Nobert Momoh <okhai0112@gmail.com>
2020-02-22 16:38:51 +0100

    Updates

---
+++ precache-manifest.6145f17a550a60f2e62fea34d76d870c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d8bab129bb8ef14f14e40091759f8875",
+    "url": "/react-shop/index.html"
+  },
+  {
+    "revision": "1226a425777a5a9be388",
+    "url": "/react-shop/static/css/main.8ebd4b1f.chunk.css"
+  },
+  {
+    "revision": "18cf51073fdf468809ec",
+    "url": "/react-shop/static/js/2.ae0e11ca.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/react-shop/static/js/2.ae0e11ca.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1226a425777a5a9be388",
+    "url": "/react-shop/static/js/main.3cd1cf95.chunk.js"
+  },
+  {
+    "revision": "dc380deb3f1bc5df933e",
+    "url": "/react-shop/static/js/runtime-main.ea569023.js"
+  }
+]);
