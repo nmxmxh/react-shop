6edcc6844fbf8fdaa05647316dab699b6fff5689
Nobert Momoh <okhai0112@gmail.com>
2020-02-25 15:28:15 +0100

    redirect from the homepage extension for github pages

--- src/App.js
+++ src/App.js
@@ -44,6 +44,7 @@ class App extends React.Component {
       <div>
       <Header />
       <Switch>
+          <Route exact path='/react-shop' render={() => <Redirect to='/' />} />
           <Route exact path='/' component={HomePage} />
           <Route path='/shop' component={ShopPage} />
           <Route exact path='/signin' render ={() =>  this.props.currentUser ? (<Redirect to='/' />) : (<SignInAndSignUpPage />)} />
