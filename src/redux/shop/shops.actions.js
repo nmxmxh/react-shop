a2c80ba4206ab3c9e477a2ab98001b28ad7fe45f
Nobert Momoh <okhai0112@gmail.com>
2020-03-02 01:33:40 +0100

    Used Higher Order Functions to simplify shop component, used thunk and asychronous JS for effective promise handling

--- src/redux/shop/shops.actions.js
+++ src/redux/shop/shops.actions.js
@@ -1,6 +1,31 @@
 import ShopActionTypes from './shops.types';
 
-export const updateCollections = (collectionsMap) => ({
-    type: ShopActionTypes.UPDATE_COLLECTIONS,
-    payload: collectionsMap
+import { firestore, convertCollectiondSnapshotToMap } from '../../firebase/firebase.utils';
+
+
+
+export const fetchCollectionsStart = () => ({
+    type: ShopActionTypes.FETCH_COLLECTIONS_FAILURE, 
 });
+
+export const fetchCollectionsStartAsync = () => {
+    return dispatch => {
+        const collectionRef = firestore.collection('collections');
+        dispatch(fetchCollectionsStart());
+
+        collectionRef.get().then(snapshot => {
+            const collectionsMap = convertCollectiondSnapshotToMap(snapshot);
+            dispatch(fetchCollectionsSuccess(collectionsMap))
+        }).catch(error => dispatch(fetchCollectionsFailure(error.message)))
+    }
+}
+
+export const fetchCollectionsSuccess = collectionsMap => ({
+    type: ShopActionTypes.FETCH_COLLECTIONS_SUCCESS,
+    payload: collectionsMap
+})
+
+export const fetchCollectionsFailure = errorMessage => ({
+    type: ShopActionTypes.FETCH_COLLECTIONS_FAILURE,
+    payload: errorMessage
+})
